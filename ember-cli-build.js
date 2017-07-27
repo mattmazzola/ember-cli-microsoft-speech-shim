3627a7e48aadaa4c75dcac775c4c8bb40a9f9dd5
Matt Mazzola <mattm@microsoft.com>
2017-07-27 11:37:57 -0700

    Add code to import microsoft-speech-browser-sdk package into vendor tree and consume it in parent app or dummy app.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,6 +14,7 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
+  app.import('vendor/speech.browser.sdk.js');
 
   return app.toTree();
 };
--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 /* eslint-env node */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-cli-microsoft-speech-shim'
+  name: 'ember-cli-microsoft-speech-shim',
+
+  isDevelopingAddon: function () {
+    return true;
+  },
+
+  included: function (app) {
+    this._super.included.apply(this, arguments);
+
+    app.import('vendor/speech.browser.sdk.js');
+  },
+
+  treeForVendor(vendorTree) {
+    var momentTree = new Funnel(path.dirname(require.resolve('microsoft-speech-browser-sdk/distrib/speech.browser.sdk.js')), {
+      files: [
+        'speech.browser.sdk-min.js',
+        'speech.browser.sdk.js',
+        'speech.browser.sdk.js.map',
+      ],
+    });
+
+    return new MergeTrees([vendorTree, momentTree]);
+  },
 };
